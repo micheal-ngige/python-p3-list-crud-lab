b2adfa7b8e8675fe2f5b82575e372b31cb433c3e
michael <michaelngige@gmail.com>
2023-11-25 12:45:01 +0300

    passing tests

--- lib/list_crud.py
+++ lib/list_crud.py
@@ -1,26 +1,45 @@
 def create_an_empty_list():
-    return None
+    return []
 
 def create_a_list():
-    return None
+    return [1,2,3,4]
 
 def add_element_to_end_of_list(l, element):
-    return None
+    create_list= [1,2,3,4]
+    create_list.append(5)
+    return create_list
 
 def add_element_to_start_of_list(l, element):
-    return None
+    list_one= [1,2,3,4,5]
+    list_one.insert(0,0)
+    return list_one
 
 def remove_element_from_end_of_list(l):
-    return None
+    remove_list= [1,2,3,4]
+    remove_list.pop()
+    return remove_list
 
 def remove_element_from_start_of_list(l):
-    return None
+    remove_list2= [1,2,3,4]
+    del remove_list2[0]
+    return remove_list2
 
 def retrieve_first_element_from_list(l):
-    return None
+   
+    if len(l) > 0:
+        return l[0]
+    else:
+        return None  
+
 
 def retrieve_element_from_index(l, index):
-    return None
+    if len(l) > 1:
+        return l[1]
+    else:
+        return None  
+    
 
 def retrieve_last_element_from_list(l):
-    return None
+    last_el=[1,2,3,4]
+    new_list= last_el[-1]
+    return new_list
